a76b8167f3c64227759a5a52e9818fbff95f7672
Alena Zueva <aleo.zueva@gmail.com>
2019-09-17 18:15:52 +0700

    js libs added

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('.owl1').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    responsive: false,
+    items:3
+})
+
+$('.owl2').owlCarousel({
+    loop:true,
+    margin:40,
+    nav:false,
+    responsive: false,
+    items:1
+})
+
