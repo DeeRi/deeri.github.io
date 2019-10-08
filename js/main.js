8c052f91114dfdff5814c4417a3a3e148ff13c8a
Alena Zueva <aleo.zueva@gmail.com>
2019-10-08 12:32:02 +0700

    animation to burger navigation added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,28 @@
-$('.owl1').owlCarousel({
-    loop:true,
-    margin:30,
-    nav:false,
-    responsive: false,
-    items:3
-})
+$(".owl1").owlCarousel({
+  loop: true,
+  margin: 30,
+  nav: false,
+  responsive: false,
+  items: 3
+});
 
-$('.owl2').owlCarousel({
-    loop:true,
-    margin:40,
-    nav:false,
-    responsive: false,
-    items:1
-})
+$(".owl2").owlCarousel({
+  loop: true,
+  margin: 40,
+  nav: false,
+  responsive: false,
+  items: 1
+});
 
+var nav = document.querySelector(".navigation");
+var hamburger = document.querySelector(".menu-button");
+
+hamburger.addEventListener("click", function() {
+  if (nav.classList.contains("navigation-close")) {
+    nav.classList.remove("navigation-close");
+    nav.classList.add("navigation-open");
+  } else {
+    nav.classList.remove("navigation-open");
+    nav.classList.add("navigation-close");
+  }
+});
